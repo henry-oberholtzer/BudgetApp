220e9b05f7c8b355b2cb3cabfb27779bc93e742c
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-02 10:35:27 -0800

    passing tests up to test_get_balance

--- budget.py
+++ budget.py
@@ -1,7 +1,31 @@
 class Category:
+  def __init__(self, name):
+    self.name = name
+    self.ledger = []
+
+  def deposit(self, amount, description=""):
+    self.ledger.append({"amount": amount, "description": description})
+  
+  def withdraw(self, amount, description=""):
+    self.ledger.append({"amount": -amount, "description": description})
+
+  def get_balance(self):
+    balance = 0
+    for entry in self.ledger:
+      balance += entry["amount"]
+    return balance
+
+  def transfer(self, amount, category):
+    pass
+
+  def check_funds(self, amount):
+    pass
+
+  
 
 
 
 
 
 def create_spend_chart(categories):
+  pass
